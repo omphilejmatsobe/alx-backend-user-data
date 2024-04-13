2309ffedf543660d07e74f96851a8da86c756616
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-13 19:26:51 +0200

    filter function update

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for Personal Data handling
+"""
+
+from typing import List
+import re
+
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
