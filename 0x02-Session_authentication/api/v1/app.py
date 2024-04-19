873b9489296bcb8215b003e839d8644dcbf35628
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-19 12:55:31 +0200

    App.py file updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,16 +13,23 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-
 auth = None
 
-if os.getenv("AUTH_TYPE") == "auth":
+if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif os.getenv("AUTH_TYPE") == "basic_auth":
+elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif getenv('AUTH_TYPE') == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif getenv('AUTH_TYPE') == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -46,20 +53,24 @@ def forbidden_error(error) -> str:
 
 
 @app.before_request
-def before_request() -> str:
+def before_request() -> None:
     """ Filter for request
     """
     request_path_list = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/']
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, request_path_list):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
                 abort(401)
+            request.current_user = auth.current_user(request)
             if auth.current_user(request) is None:
                 abort(403)
-
+            if request.current_user is None:
+                abort(403)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
