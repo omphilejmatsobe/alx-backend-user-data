98f8b02a3b6b2d87c271b4ea172433801e5ed686
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-19 13:05:43 +0200

    App.py file updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,29 +10,18 @@ import os
 
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-auth = None
-AUTH_TYPE = getenv("AUTH_TYPE")
 
-if AUTH_TYPE == "auth":
+auth = None
+
+if os.getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+elif os.getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-elif AUTH_TYPE == "session_exp_auth":
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
-elif AUTH_TYPE == "session_db_auth":
-    from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -57,23 +46,18 @@ def forbidden_error(error) -> str:
 
 
 @app.before_request
-def before_request() -> None:
+def before_request() -> str:
     """ Filter for request
     """
     request_path_list = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/']
+        '/api/v1/forbidden/']
     if auth:
         if auth.require_auth(request.path, request_path_list):
-            if auth.authorization_header(
-                    request) is None and auth.session_cookie(request) is None:
+            if auth.authorization_header(request) is None:
                 abort(401)
-            request.current_user = auth.current_user(request)
             if auth.current_user(request) is None:
-                abort(403)
-            if request.current_user is None:
                 abort(403)
 
 
