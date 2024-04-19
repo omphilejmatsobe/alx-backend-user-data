48d8dfcf39596c2de2367b0acabc698f105faff9
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-19 15:14:52 +0200

    File check

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,6 +2,7 @@
 """
 Route module for the API
 """
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -12,21 +13,22 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-AUTH_TYPE = os.getenv("AUTH_TYPE")
-if AUTH_TYPE == "auth":
+AUTH = os.getenv("AUTH_TYPE")
+if AUTH == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+elif AUTH == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif AUTH_TYPE == "session_auth":
+elif AUTH == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-elif AUTH_TYPE == "session_exp_auth":
+elif AUTH == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
-elif AUTH_TYPE == "session_db_auth":
+elif AUTH == "session_db_auth":
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
 
@@ -40,13 +42,13 @@ def bef_req():
         pass
     else:
         setattr(request, "current_user", auth.current_user(request))
-        excluded = [
+        paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
             '/api/v1/auth_session/login/'
         ]
-        if auth.require_auth(request.path, excluded):
+        if auth.require_auth(request.path, paths):
             cookie = auth.session_cookie(request)
             if auth.authorization_header(request) is None and cookie is None:
                 abort(401, description="Unauthorized")
