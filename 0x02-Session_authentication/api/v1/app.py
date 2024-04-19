8e75366c7885412b50df61651a3e5d4571d64eac
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-19 13:02:49 +0200

    App.py file updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -76,6 +76,7 @@ def before_request() -> None:
             if request.current_user is None:
                 abort(403)
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -12,8 +12,8 @@ def view_all_users() -> str:
     Return:
       - list of all User objects JSON represented
     """
-    all_users = [user.to_json() for user in User.all()]
-    return jsonify(all_users)
+    users = [user.to_json() for user in User.all()]
+    return jsonify(users)
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
@@ -25,16 +25,20 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
+
     if user_id is None:
         abort(404)
-    if user_id == 'me':
-        if request.current_user is None:
-            abort(404)
-        else:
-            return jsonify(request.current_user.to_json())
+
+    if user_id == "me" and request.current_user is None:
+        abort(404)
+
+    if user_id == "me" and request.current_user is not None:
+        return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
+
     return jsonify(user.to_json())
 
 
@@ -47,6 +51,7 @@ def delete_user(user_id: str = None) -> str:
       - empty JSON is the User has been correctly deleted
       - 404 if the User ID doesn't exist
     """
+
     if user_id is None:
         abort(404)
     user = User.get(user_id)
@@ -68,8 +73,10 @@ def create_user() -> str:
       - User object JSON represented
       - 400 if can't create the new User
     """
+
     rj = None
     error_msg = None
+
     try:
         rj = request.get_json()
     except Exception as e:
@@ -91,6 +98,7 @@ def create_user() -> str:
             return jsonify(user.to_json()), 201
         except Exception as e:
             error_msg = "Can't create User: {}".format(e)
+
     return jsonify({'error': error_msg}), 400
 
 
@@ -107,12 +115,15 @@ def update_user(user_id: str = None) -> str:
       - 404 if the User ID doesn't exist
       - 400 if can't update the User
     """
+
     if user_id is None:
         abort(404)
     user = User.get(user_id)
     if user is None:
         abort(404)
+
     rj = None
+
     try:
         rj = request.get_json()
     except Exception as e:
@@ -124,4 +135,5 @@ def update_user(user_id: str = None) -> str:
     if rj.get('last_name') is not None:
         user.last_name = rj.get('last_name')
     user.save()
+
     return jsonify(user.to_json()), 200
