c8a466ef1328cd816404d6c6bd959ddb56573537
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-19 15:26:10 +0200

    File test

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-""" UserSession module
+""" User Session Module
 """
 from models.base import Base
+from typing import TypeVar, List, Iterable
+from os import path
+import json
+import uuid
 
 
 class UserSession(Base):
-    """
-    UserSession class
-    """
-
+    """ Class User Session """
     def __init__(self, *args: list, **kwargs: dict):
-        """
-        Initialize a UserSession instance
-        """
+        """ Initializes UserSession """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
