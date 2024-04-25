21619c2213de77fcc3103c8183b1daa8fd6c6a75
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-25 20:04:26 +0200

    db test

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 
 """
-Database class
+Databas class
 """
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import Base, User
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
+from user import Base, User
 
 class DB:
     """
