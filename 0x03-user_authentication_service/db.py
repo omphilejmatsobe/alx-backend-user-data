bae7cd2b80a741592fa6c3d5271fe1b24800c032
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-25 19:27:38 +0200

    File test

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -38,7 +38,13 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Method saves new user to the database"""
+        
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        try:
+            self._session.add(user)
+            self._session.commit()
+        except Exception as e:
+            print(f"Error adding user to database: {e}")
+            self._session.rollback()
+            raise
         return user
