49d7b53dc60ec771e45fc4328cd505f2e8df2fd7
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-25 20:28:47 +0200

    File check

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -65,10 +65,6 @@ class DB:
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """ Searches for user instance using given id parameter
-            Args:
-                - user_id: user's id
-            Return:
-                - User instance found
         """
         user = self.find_user_by(id=user_id)
         session = self._session
